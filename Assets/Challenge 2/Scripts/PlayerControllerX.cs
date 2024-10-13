d05939a2ba1c1eb68a8590565d48108387bea657
Franklin Addo <B00159369@mytudublin.ie>
2024-10-13 18:02:54 +0100

    ChallengeFix

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,12 +6,16 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float fireRate = 1;
+    private float nextFire = 0; 
+
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press, send dog and the time between each spacebar pressed 
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -23,10 +23,12 @@ public class SpawnManagerX : MonoBehaviour
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
+        int index = Random.Range(0, ballPrefabs.Length);
+        spawnInterval = Random.Range(2,4);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
     }
 
 }
